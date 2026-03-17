30e568a63bb04151bbca4e0b220883fd50a75c5c
kimheffy <kimjeffrey96@gmail.com>
2026-03-16 19:48:48 -0700

    init commit
    - added axum
    - postgres
    - able to add user

---
+++ axum-api/src/main.rs
@@ -0,0 +1,75 @@
+use std::sync::Arc;
+
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+struct AppState {
+    pool: sqlx::PgPool,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let database_url = "postgres://postgres:postgres@localhost/zenith";
+
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await?;
+
+    let shared_state = Arc::new(AppState { pool });
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/register", post(register_handler))
+        .with_state(shared_state);
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct RegisterUser {
+    username: String,
+    email: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: Uuid,
+    username: String,
+    email: String,
+}
+
+async fn root() -> &'static str {
+    "Hello world!"
+}
+
+async fn register_handler(
+    State(state): State<Arc<AppState>>,
+    Json(payload): Json<RegisterUser>,
+) -> (StatusCode, Json<User>) {
+    let id = Uuid::new_v4();
+
+    sqlx::query!(
+        "INSERT INTO users (id, username, email) VALUES ($1, $2, $3)",
+        id,
+        payload.username,
+        payload.email
+    )
+    .execute(&state.pool)
+    .await;
+
+    let user = User {
+        id,
+        username: payload.username,
+        email: payload.email,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
