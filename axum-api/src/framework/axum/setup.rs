6e8ab118b98931f4f8d4728229ddaa5694ef4bde
kimheffy <kimjeffrey96@gmail.com>
2026-03-20 01:05:48 -0700

    remove unused imports, deleted comments

--- axum-api/src/framework/axum/setup.rs
+++ axum-api/src/framework/axum/setup.rs
@@ -1,9 +1,6 @@
-use std::sync::Arc;
-
 use crate::framework::axum::app_state::AppState;
 use crate::framework::postgres::postgres_persistence;
-
-use crate::infrastructure::repository::user_repo;
+use std::sync::Arc;
 
 pub async fn init_app() -> anyhow::Result<AppState> {
     // TODO: load in the config
@@ -12,8 +9,6 @@ pub async fn init_app() -> anyhow::Result<AppState> {
     let postgres_arc = Arc::new(postgres_persistence().await?);
 
     // <dependency injections>
-    // let user_repo = UserRepository::new(postgres_arc);
-
     let shared_state = AppState {
         user_repo: postgres_arc.clone(),
     };
