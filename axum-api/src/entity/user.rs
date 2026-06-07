bd0ca6477e265b41cbbe51bffe8624221c05c47d
kimheffy <kimjeffrey96@gmail.com>
2026-06-06 17:29:34 -0700

    fix workflow yml, remove unused code

--- axum-api/src/entity/user.rs
+++ axum-api/src/entity/user.rs
@@ -1,15 +1,4 @@
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
-#[derive(Serialize)]
-struct User {
-    id: Uuid,
-    username: String,
-    email: String,
-    // NOTE: add password_hash back when implementing jwt
-    password_hashed: String,
-    created_at: u64,
-}
+use serde::Deserialize;
 
 #[derive(std::fmt::Debug, Deserialize)]
 pub struct RegisterUserRequest {
